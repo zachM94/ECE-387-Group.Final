d98200592034a0e21b3aca8ac6add6f307a19166
guow3 <guow3@miamioh.edu>
2016-03-10 23:51:26 -0500

    Added files via upload

---
+++ CustomReceiver.ino
@@ -0,0 +1,42 @@
+//#include <DigitalIO.h>
+#include <SPI.h>
+#include "RF24.h"
+#include <printf.h>
+RF24 radio(2,3);
+byte pipe = 0xE8E8F0F0E1LL;
+unsigned long msg;
+
+void setup() {
+    Serial.begin(115200);
+    printf_begin();
+    Serial.println("Receiver");
+    pinMode(9, OUTPUT);
+    radio.begin();
+    radio.setChannel(108);
+    radio.openReadingPipe(0,pipe);
+    radio.printDetails();
+ 
+}
+
+void loop() {
+   radio.startListening();
+   digitalWrite(9, LOW);
+  if(radio.available()){
+    while(radio.available())
+    {
+    radio.read(&msg, sizeof(msg));
+    Serial.println("Incoming...");
+    delay(500);
+    Serial.println(msg);
+    }
+  }
+  else{
+    Serial.println("No Msg");
+  }
+  Serial.println(msg);
+  if (msg == 835716){
+    digitalWrite(9, HIGH);
+    Serial.println(msg);
+  }
+
+}
---
+++ CustomTransmitter.ino
@@ -0,0 +1,33 @@
+
+#include <SPI.h>
+#include "RF24.h"
+#include <printf.h>
+RF24 radio(7,8);
+byte pipe = 0xE8E8F0F0E1LL;
+byte msg = 11;
+
+void setup() {
+    Serial.begin(115200);
+    printf_begin();
+    Serial.println("Transmitter");
+    pinMode(9, INPUT);
+    radio.begin();
+    radio.setChannel(108);
+    radio.openWritingPipe(pipe);
+    radio.startListening();
+    radio.printDetails();
+}
+
+void loop() {
+  radio.stopListening();
+  if (digitalRead(9) == HIGH)
+  {
+    while (!radio.write(&msg, sizeof(msg))){radio.write(&msg, sizeof(msg));}
+  Serial.println("Sending.....");
+  }
+  else {
+    Serial.println("Awaiting Input....");
+  }
+  delay(500); 
+
+}
