f4e31a8650f573f4ba26dc4c6fcc417df88e7ec2
zachM94 <zach.mcquigg@gmail.com>
2016-04-04 20:06:46 -0400

    stuff

--- RF/Receiver_v2/Receiver_v2.ino
+++ RF/Receiver_v2/Receiver_v2.ino
@@ -6,20 +6,18 @@
 */
 
 #include <SPI.h>
+#include <SD.h>
 #include "RF24.h"
 
 /****************** User Config ***************************/
 /***      Set this radio as radio number 0 or 1         ***/
 bool radioNumber = 0;
-
+File aFile;
 /* Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 7 & 8 */
 RF24 radio(7,8);
 /**********************************************************/
 
 byte addresses[][6] = {"1Node","2Node"};
-
-// Used to control whether this node is sending or receiving
-bool role = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -30,6 +28,16 @@ void setup() {
   pinMode(2,OUTPUT);
   pinMode(3,OUTPUT);
   pinMode(4,OUTPUT);  
+  //set up SD card
+  pinMode(10, OUTPUT);
+
+  if (!SD.begin(10)) {
+    Serial.println("SD Failed!");
+    return;
+  }
+  else {
+    Serial.println("SD READY!");
+  }
 
 
   // Set the PA Level low to prevent power supply related issues since this is a
@@ -51,60 +59,8 @@ void setup() {
 }
 
 void loop() {
-  
-  
-/****************** Ping Out Role ***************************/  
-if (role == 1)  {
-    
-    radio.stopListening();                                    // First, stop listening so we can talk.
-    
-    
-    Serial.println(F("Now sending"));
-
-    unsigned long start_time = micros();                             // Take the time, and send it.  This will block until complete
-     if (!radio.write( &start_time, sizeof(unsigned long) )){
-       Serial.println(F("failed"));
-     }
-        
-    radio.startListening();                                    // Now, continue listening
-    
-    unsigned long started_waiting_at = micros();               // Set up a timeout period, get the current microseconds
-    boolean timeout = false;                                   // Set up a variable to indicate if a response was received or not
-    
-    while ( ! radio.available() ){                             // While nothing is received
-      if (micros() - started_waiting_at > 200000 ){            // If waited longer than 200ms, indicate timeout and exit while loop
-          timeout = true;
-          break;
-      }      
-    }
-        
-    if ( timeout ){                                             // Describe the results
-        Serial.println(F("Failed, response timed out."));
-    }else{
-        unsigned long got_time;                                 // Grab the response, compare, and send to debugging spew
-        radio.read( &got_time, sizeof(unsigned long) );
-        unsigned long end_time = micros();
-        
-        // Spew it
-        Serial.print(F("Sent "));
-        Serial.print(start_time);
-        Serial.print(F(", Got response "));
-        Serial.print(got_time);
-        Serial.print(F(", Round-trip delay "));
-        Serial.print(end_time-start_time);
-        Serial.println(F(" microseconds"));
-    }
-
-    // Try again 1s later
-    delay(1000);
-  }
-
-
 
 /****************** Pong Back Role ***************************/
-
-  if ( role == 0 )
-  {
     unsigned long got_time;
     unsigned long msg;    
     
@@ -138,30 +94,25 @@ if (role == 1)  {
         digitalWrite(4,LOW);         
       }
       Serial.print(F("Sent response "));
-      Serial.println(got_time);  
+      Serial.println(msg);  
+      
+      aFile = SD.open("aFile.txt", FILE_WRITE);
+      if (aFile){
+        Serial.println("Writing to File");
+        aFile.println(msg);
+        aFile.close();  
+      }
+      else {
+        Serial.println("Could not write to file");
+        aFile.close();
+      }
    }
- }
+ 
 
 
 
 
 /****************** Change Roles via Serial Commands ***************************/
-
-  if ( Serial.available() )
-  {
-    char c = toupper(Serial.read());
-    if ( c == 'T' && role == 0 ){      
-      Serial.println(F("*** CHANGING TO TRANSMIT ROLE -- PRESS 'R' TO SWITCH BACK"));
-      role = 1;                  // Become the primary transmitter (ping out)
-    
-   }else
-    if ( c == 'R' && role == 1 ){
-      Serial.println(F("*** CHANGING TO RECEIVE ROLE -- PRESS 'T' TO SWITCH BACK"));      
-       role = 0;                // Become the primary receiver (pong back)
-       radio.startListening();
-       
-    }
-  }
 
 
 } // Loop
